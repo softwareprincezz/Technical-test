83e747a1cb98720752494b6aab8e2222284dff8c
SharonBarrial <btsgaveme2wings@gmail.com>
2026-03-04 17:26:31 -0500

    feat: add api deployed

--- react-orders/vite.config.js
+++ react-orders/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://technical-test-production-ea39.up.railway.app',
         changeOrigin: true
       }
     }
